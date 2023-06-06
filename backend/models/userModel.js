73b7339c0db4caa1a80c02197ec7f16b131551d2
Hasan Rashed <hasanrashed537@gmail.com>
2023-06-06 08:02:05 +0600

    hash password in register user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 
 const userSchema = mongoose.Schema({
@@ -17,9 +18,18 @@ const userSchema = mongoose.Schema({
     },
 },
 {
-    timestamps: true
+    timestamps: true // to get created at and updated at by default
 });
 
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){ // this keyword refers to the user we are trying to save in the User model or document
+        next(); // if password is not modified, we don't want to do anything and move on
+    }
+
+    const salt = await bcrypt.genSalt(10); // generate salt with 10 rounds
+    this.password = await bcrypt.hash(this.password, salt); // hash the password with the salt. this.password refers to the password in the User model or document
+});
 
 const User = mongoose.model('User', userSchema);
 
