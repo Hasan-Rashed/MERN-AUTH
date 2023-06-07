30b913797b72d7966421be92061c308b266e43f6
Hasan Rashed <hasanrashed537@gmail.com>
2023-06-07 10:47:06 +0600

    viteJs setup with tailwind and material

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-magicBlue container mx-auto text-center font-bold underline "> Material UI </h1>
+    </>
+
+  )
+}
+
+
+export default App
