69f1adf086ead699efa6b9b192afd4ae643cd85d
Hasan Rashed <hasanrashed537@gmail.com>
2023-06-07 07:34:02 +0600

    Auth protect middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express  from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000  ;
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(express.json()); // to accept json data in the body
 app.use(express.urlencoded({ extended: true })); // allow us to send form data
+
+app.use(cookieParser()); // to accept cookies in the browser
 
 app.use('/api/users', userRoutes);
 
