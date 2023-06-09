64cb1665e857a9f869040357d9941ee83b7e2422
Hasan Rashed <hasanrashed537@gmail.com>
2023-06-10 00:11:07 +0600

    Api Slices

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
     reducer: {
         // auth is the name of the state or slice and authReducer is the reducer function
-        auth: authReducer
+        auth: authReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
 });
 
